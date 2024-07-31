825e547ea2665959a0d6952b832eb8d2bec527cb
Tema <a.d.yakovenko@yandex.ru>
2024-07-31 18:54:21 +0300

    errors fix 1

--- module_2_4.py
+++ module_2_4.py
@@ -1,16 +1,16 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes, not_primes = [], []
-for ind in range(len(numbers)):
-    if numbers[ind] == 1:
+for number in numbers:
+    if number == 1:
         continue
-    max_val_to_check = numbers[ind] // 2
+    max_val_to_check = number // 2
     is_prime = True
     for divider in range(2, max_val_to_check + 1):
-        if numbers[ind] % divider == 0:
+        if number % divider == 0:
             is_prime = False
-            not_primes.append(numbers[ind])
+            not_primes.append(number)
             break
     if is_prime:
-        primes.append(numbers[ind])
+        primes.append(number)
 print('Primes:', primes)
 print('Not Primes:', not_primes)
