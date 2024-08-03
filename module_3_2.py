dd860c8e9c4da3cafd57edfdbad4ecf6e0cbbbf2
Tema <a.d.yakovenko@yandex.ru>
2024-08-03 13:10:16 +0300

    initial commit

---
+++ module_3_2.py
@@ -0,0 +1,22 @@
+default_sender = 'university.help@gmail.com'
+
+
+def send_email(message, recipient, sender=default_sender):
+    if not is_possible_address(sender) or not is_possible_address(recipient):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == default_sender:
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+
+
+def is_possible_address(address):
+    return address.endswith(('.com', '.ru', '.net')) and r'@' in address
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
