8012ca626fb888bd57feba44169949511672c00e
Tema <a.d.yakovenko@yandex.ru>
2024-08-04 19:22:14 +0300

    initial commit

---
+++ module_4_1/true_math.py
@@ -0,0 +1,11 @@
+def divide(first, second):
+    return first / second if second != 0 else float('inf')
+
+
+def main():
+    print(divide(1, 2))
+    print(divide(1, 0))
+
+
+if __name__ == '__main__':
+    main()
