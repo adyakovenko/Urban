6daef963447c9689164bd629f43a979c20e9062a
Tema <a.d.yakovenko@yandex.ru>
2024-12-06 00:40:12 +0300

    initial

---
+++ module_14_2/module_14_2.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+cursor.execute('DELETE from Users')
+
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)', (f'User{i}',f'example{i}@gmail.com', i * 10, 1000))
+
+for i in range(1, 11, 2):
+    cursor.execute('UPDATE Users SET balance = ? WHERE id = ?', (500, i))
+
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM Users WHERE id = ?', (i,))
+
+# cursor.execute(('SELECT * FROM Users WHERE age != ?'), (60,))
+# users = cursor.fetchall()
+# for user in users:
+#     print(f'Имя: {user[1]} | Почта: {user[2]} | Возраст: {user[3]} | Баланс: {user[4]}')
+
+cursor.execute('DELETE FROM Users WHERE id = 6')
+cursor.execute('SELECT COUNT(*) FROM Users')
+n_records = cursor.fetchone()[0]
+cursor.execute('SELECT SUM(balance) FROM Users')
+balance_total = cursor.fetchone()[0]
+print(balance_total / n_records)
+
+connection.commit()
+connection.close()
