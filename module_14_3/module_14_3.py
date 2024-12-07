ce58a02e200cc8713186ca7632b3c979941d0354
Tema <a.d.yakovenko@yandex.ru>
2024-12-07 17:33:39 +0300

    initial

--- module_14_3/module_14_3.py
+++ module_14_3/module_14_3.py
@@ -6,7 +6,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = ''
+api = ' '
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
